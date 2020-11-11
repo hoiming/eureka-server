d445fa53e3c78be659e1e1b840d8804ad03633e5
haiming <haiming_liang@hotmail.com>
2020-11-11 23:04:54 +0800

    first commit, runnable eureka server: Haiming

---
+++ src/test/java/com/haiming/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haiming.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
